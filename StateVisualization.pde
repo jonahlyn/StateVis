9ccfc612ef3f0544d87ac0fee8a246e3068aa26b
Jonahlyn Gilstrap <jgilstrap@gmail.com>
2013-07-18 15:51:10 -0600

    Got some basic hover effect working.

--- StateVisualization.pde
+++ StateVisualization.pde
@@ -1,28 +1,21 @@
-XML xml;
-PShape ps;
-PShape test;
+//import processing.xml.*;
+import geomerative.*;
+
+RShape us;
 Table data;
 int rowCount;
 float dataMin = MAX_FLOAT;
 float dataMax = MIN_FLOAT;
-Scale scale;
+//Scale scale;
 PFont f;
 
 void setup() {
 	f = createFont("Arial", 10, true);
+	textFont(f);
 
 	// Get the data
 	data = new Table("nestegg-index");
 	rowCount = data.getRowCount();
-
-	// Get the map
-	// xml = loadXML("Blank_US_Map.svg");
-	// bindSVG(xml);
-	// ps = new PShapeSVG(xml);
-
-	test = loadShape("Blank_US_Map.svg");
-	size(int(test.width) + 20, int(test.height) + 20);
-	println("Width: " + test.width + " Height: " + test.height);
 
 	// Find the minimum and maximum data values
 	for (int row = 0; row < rowCount; row++) {
@@ -35,7 +28,15 @@ void setup() {
 		}
 	}
 
-	scale = new Scale(dataMin, dataMax);
+	// // Create a scale object
+	// scale = new Scale(dataMin, dataMax);
+
+	// Create a Geomerative shape
+	RG.init(this);
+	us = RG.loadShape("Blank_US_Map.svg");
+
+	// Set the size of the window
+	size(int(us.width), int(us.height));
 
 }
 
@@ -44,102 +45,118 @@ void draw() {
 
 	background(#ffffff);
 
-	strokeWeight(1);
-	stroke(#D9D9D9); //stroke(#2CA25F);
+	// strokeWeight(1);
+	// stroke(#D9D9D9); //stroke(#2CA25F);
 
-	fill(#f2f2f2);
-	rectMode(CENTER);
-	rect(width/2, test.height/2 + 10, test.width, test.height);
+	// // Draw a rectange around the graphic
+	// fill(#f2f2f2);
+	// rectMode(CENTER);
+	// rect(width/2, test.height/2 + 10, test.width, test.height);
 
-	smooth();
-	fill(0);
-	textSize(35);
-	textAlign(CENTER);
-	text("U.S. Savings by State", width/2, 50);
+	// // Heading
+	// smooth();
+	// fill(0);
+	// textSize(35);
+	// textAlign(CENTER);
+	// text("U.S. Savings by State", width/2, 50);
 
-	stroke(#BDBDBD); //stroke(#2CA25F);
+	// Get the location of the mouse
+	RPoint p = new RPoint(mouseX, mouseY);
 
-	for (int row = 0; row < rowCount; row++) {
-		String abbrev = data.getRowName(row);
-		PShape state = test.getChild(abbrev);
+	// Iterate through the states shapes
+	RShape states = us.children[0];
+	for(int i=0;i<states.countChildren();i++) {
+		RShape state = states.children[i];
+		String code = state.name;
+		float value;
 
+		try{
+			value = data.getFloat(code, 2);
+		} catch (Exception e) {
+			value = 0;
+		}
+		
+		RG.ignoreStyles(true);
+		float amt = norm(value, dataMin, dataMax);
+		color c = lerpColor(#E5F5F9, #2CA25F, amt);
+		fill(c);
+		strokeWeight(1);
+		stroke(#EFEFEF);
 
-		if (state != null) {
-			state.disableStyle();
-			float value = data.getFloat(row, 2);
-			float amt = norm(value, dataMin, dataMax);
-			color c = lerpColor(#E5F5F9, #2CA25F, amt);
-			fill(c);
+		// Draw the state shape
+		state.draw();
 
-			shape(state, (test.width*.125), 75, test.width - (test.width*.25), test.height - (test.height*.25));
-			//shape(state, 0, 0);
+		// If the mouse is over the state
+		if(states.children[i].contains(p)) {
+			RPoint center = state.getCenter();
+
+			RG.ignoreStyles(true);
+
+			pushMatrix();
+			stroke(10,10,10,150);
+			strokeWeight(1);
+			state.draw();
+			popMatrix();
+			
+			//text overlay
+			strokeWeight(1);
+			fill(10,10,10,150);
+			rectMode(CENTER);
+			rect(center.x, center.y, 75, 30);
+			fill(255);
+			textSize(10);
+			textAlign(CENTER);
+			text(code + " " + value, center.x, center.y+5);
 		}
 	}
+
 
 	// Draw the scale
-	int x = int(test.width) - 175;
-	int y = int(test.height) - 25;
-	scale.draw(x, y);
-}
-
-void bindSVG(XML svg) {
-	XML[] paths = svg.getChild("g").getChildren("path");
-	
-	for(int i = 0; i < paths.length; i++){
-		String code = paths[i].getString("id");
-		//Float value = data.getFloat(code, 2);
-
-		if(code.length() > 2){ continue; }
-
-		try {
-			println(code + " " + data.getFloat(code, 2));
-			paths[i].setString("fill", "#61E2F0");
-		} catch (Exception e) {
-			println("Caught an error");
-		}		
-	}
+	// int x = int(test.width) - 175;
+	// int y = int(test.height) - 25;
+	// scale.draw(x, y);
 }
 
 
-class Scale {
-	float min, max;
-	int x, y;
+// class Scale {
+// 	float min, max;
+// 	int x, y;
 
-	Scale(float dataMin, float dataMax){
-		min = dataMin;
-		max = dataMax;
-	}
+// 	Scale(float dataMin, float dataMax){
+// 		min = dataMin;
+// 		max = dataMax;
+// 	}
 
-	void draw(int startx, int starty){
-		x = startx;
-		y = starty;
+// 	void draw(int startx, int starty){
+// 		x = startx;
+// 		y = starty;
 
-		// Draw text markings
-		drawText(""+dataMin, x+10, y-25);
+// 		// Draw text markings
+// 		drawText(""+dataMin, x+10, y-25);
 
-		// Draw the scale image
-		for(float i = dataMin; i <= dataMax; i += 1.0) {
-			color c = lerpColor(#E5F5F9, #2CA25F, norm(i, dataMin, dataMax));
-			fill(c);
-			stroke(c);
-			rect(x, y, 5, 25);
-			x += 5;
-		}
+// 		// Draw the scale image
+// 		for(float i = dataMin; i <= dataMax; i += 1.0) {
+// 			color c = lerpColor(#E5F5F9, #2CA25F, norm(i, dataMin, dataMax));
+// 			fill(c);
+// 			stroke(c);
+// 			rect(x, y, 5, 25);
+// 			x += 5;
+// 		}
 
-		// Draw text markings
-		drawText(""+dataMax, x, y-25);
+// 		// Draw text markings
+// 		drawText(""+dataMax, x, y-25);
 
-	}
+// 	}
 
-	void drawText(String text, int x, int y){
-		pushMatrix();
-		translate(x, y);
-		rotate(radians(-45));
-		fill(0);
-		textFont(f);
-		textSize(10);
-		text(text, 0, 0);
-		popMatrix();
-	}
+// 	void drawText(String text, int x, int y){
+// 		pushMatrix();
+// 		translate(x, y);
+// 		rotate(radians(-45));
+// 		fill(0);
+// 		textFont(f);
+// 		textSize(10);
+// 		text(text, 0, 0);
+// 		popMatrix();
+// 	}
 
-}
+// }
--- build-tmp/source/StateVisualization.java
+++ build-tmp/source/StateVisualization.java
@@ -3,6 +3,8 @@ import processing.data.*;
 import processing.event.*; 
 import processing.opengl.*; 
 
+import geomerative.*; 
+
 import java.util.HashMap; 
 import java.util.ArrayList; 
 import java.io.File; 
@@ -14,31 +16,24 @@ import java.io.IOException;
 
 public class StateVisualization extends PApplet {
 
-XML xml;
-PShape ps;
-PShape test;
+//import processing.xml.*;
+
+
+RShape us;
 Table data;
 int rowCount;
 float dataMin = MAX_FLOAT;
 float dataMax = MIN_FLOAT;
-Scale scale;
+//Scale scale;
 PFont f;
 
 public void setup() {
 	f = createFont("Arial", 10, true);
+	textFont(f);
 
 	// Get the data
 	data = new Table("nestegg-index");
 	rowCount = data.getRowCount();
-
-	// Get the map
-	// xml = loadXML("Blank_US_Map.svg");
-	// bindSVG(xml);
-	// ps = new PShapeSVG(xml);
-
-	test = loadShape("Blank_US_Map.svg");
-	size(PApplet.parseInt(test.width) + 20, PApplet.parseInt(test.height) + 20);
-	println("Width: " + test.width + " Height: " + test.height);
 
 	// Find the minimum and maximum data values
 	for (int row = 0; row < rowCount; row++) {
@@ -51,7 +46,15 @@ public void setup() {
 		}
 	}
 
-	scale = new Scale(dataMin, dataMax);
+	// // Create a scale object
+	// scale = new Scale(dataMin, dataMax);
+
+	// Create a Geomerative shape
+	RG.init(this);
+	us = RG.loadShape("Blank_US_Map.svg");
+
+	// Set the size of the window
+	size(PApplet.parseInt(us.width), PApplet.parseInt(us.height));
 
 }
 
@@ -60,105 +63,121 @@ public void draw() {
 
 	background(0xffffffff);
 
-	strokeWeight(1);
-	stroke(0xffD9D9D9); //stroke(#2CA25F);
+	// strokeWeight(1);
+	// stroke(#D9D9D9); //stroke(#2CA25F);
 
-	fill(0xfff2f2f2);
-	rectMode(CENTER);
-	rect(width/2, test.height/2 + 10, test.width, test.height);
+	// // Draw a rectange around the graphic
+	// fill(#f2f2f2);
+	// rectMode(CENTER);
+	// rect(width/2, test.height/2 + 10, test.width, test.height);
 
-	smooth();
-	fill(0);
-	textSize(35);
-	textAlign(CENTER);
-	text("U.S. Savings by State", width/2, 50);
+	// // Heading
+	// smooth();
+	// fill(0);
+	// textSize(35);
+	// textAlign(CENTER);
+	// text("U.S. Savings by State", width/2, 50);
 
-	stroke(0xffBDBDBD); //stroke(#2CA25F);
+	// Get the location of the mouse
+	RPoint p = new RPoint(mouseX, mouseY);
 
-	for (int row = 0; row < rowCount; row++) {
-		String abbrev = data.getRowName(row);
-		PShape state = test.getChild(abbrev);
+	// Iterate through the states shapes
+	RShape states = us.children[0];
+	for(int i=0;i<states.countChildren();i++) {
+		RShape state = states.children[i];
+		String code = state.name;
+		float value;
 
+		try{
+			value = data.getFloat(code, 2);
+		} catch (Exception e) {
+			value = 0;
+		}
+		
+		RG.ignoreStyles(true);
+		float amt = norm(value, dataMin, dataMax);
+		int c = lerpColor(0xffE5F5F9, 0xff2CA25F, amt);
+		fill(c);
+		strokeWeight(1);
+		stroke(0xffEFEFEF);
 
-		if (state != null) {
-			state.disableStyle();
-			float value = data.getFloat(row, 2);
-			float amt = norm(value, dataMin, dataMax);
-			int c = lerpColor(0xffE5F5F9, 0xff2CA25F, amt);
-			fill(c);
+		// Draw the state shape
+		state.draw();
 
-			shape(state, (test.width*.125f), 75, test.width - (test.width*.25f), test.height - (test.height*.25f));
-			//shape(state, 0, 0);
+		// If the mouse is over the state
+		if(states.children[i].contains(p)) {
+			RPoint center = state.getCenter();
+
+			RG.ignoreStyles(true);
+
+			pushMatrix();
+			stroke(10,10,10,150);
+			strokeWeight(1);
+			state.draw();
+			popMatrix();
+			
+			//text overlay
+			strokeWeight(1);
+			fill(10,10,10,150);
+			rectMode(CENTER);
+			rect(center.x, center.y, 75, 30);
+			fill(255);
+			textSize(10);
+			textAlign(CENTER);
+			text(code + " " + value, center.x, center.y+5);
 		}
 	}
+
 
 	// Draw the scale
-	int x = PApplet.parseInt(test.width) - 175;
-	int y = PApplet.parseInt(test.height) - 25;
-	scale.draw(x, y);
-}
-
-public void bindSVG(XML svg) {
-	XML[] paths = svg.getChild("g").getChildren("path");
-	
-	for(int i = 0; i < paths.length; i++){
-		String code = paths[i].getString("id");
-		//Float value = data.getFloat(code, 2);
-
-		if(code.length() > 2){ continue; }
-
-		try {
-			println(code + " " + data.getFloat(code, 2));
-			paths[i].setString("fill", "#61E2F0");
-		} catch (Exception e) {
-			println("Caught an error");
-		}		
-	}
+	// int x = int(test.width) - 175;
+	// int y = int(test.height) - 25;
+	// scale.draw(x, y);
 }
 
 
-class Scale {
-	float min, max;
-	int x, y;
+// class Scale {
+// 	float min, max;
+// 	int x, y;
 
-	Scale(float dataMin, float dataMax){
-		min = dataMin;
-		max = dataMax;
-	}
+// 	Scale(float dataMin, float dataMax){
+// 		min = dataMin;
+// 		max = dataMax;
+// 	}
 
-	public void draw(int startx, int starty){
-		x = startx;
-		y = starty;
+// 	void draw(int startx, int starty){
+// 		x = startx;
+// 		y = starty;
 
-		// Draw text markings
-		drawText(""+dataMin, x+10, y-25);
+// 		// Draw text markings
+// 		drawText(""+dataMin, x+10, y-25);
 
-		// Draw the scale image
-		for(float i = dataMin; i <= dataMax; i += 1.0f) {
-			int c = lerpColor(0xffE5F5F9, 0xff2CA25F, norm(i, dataMin, dataMax));
-			fill(c);
-			stroke(c);
-			rect(x, y, 5, 25);
-			x += 5;
-		}
+// 		// Draw the scale image
+// 		for(float i = dataMin; i <= dataMax; i += 1.0) {
+// 			color c = lerpColor(#E5F5F9, #2CA25F, norm(i, dataMin, dataMax));
+// 			fill(c);
+// 			stroke(c);
+// 			rect(x, y, 5, 25);
+// 			x += 5;
+// 		}
 
-		// Draw text markings
-		drawText(""+dataMax, x, y-25);
+// 		// Draw text markings
+// 		drawText(""+dataMax, x, y-25);
 
-	}
+// 	}
 
-	public void drawText(String text, int x, int y){
-		pushMatrix();
-		translate(x, y);
-		rotate(radians(-45));
-		fill(0);
-		textFont(f);
-		textSize(10);
-		text(text, 0, 0);
-		popMatrix();
-	}
+// 	void drawText(String text, int x, int y){
+// 		pushMatrix();
+// 		translate(x, y);
+// 		rotate(radians(-45));
+// 		fill(0);
+// 		textFont(f);
+// 		textSize(10);
+// 		text(text, 0, 0);
+// 		popMatrix();
+// 	}
 
-}
+// }
 class Table {
   int rowCount;
   String[][] data;
